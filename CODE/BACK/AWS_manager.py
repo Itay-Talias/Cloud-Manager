a71b44f85fc75fa3bcd1557068999ddccddb3390
Itay.Talias <itay.talias@gmail.com>
2022-10-25 10:08:51 +0300

    get instance info by id

--- CODE/BACK/AWS_manager.py
+++ CODE/BACK/AWS_manager.py
@@ -1,6 +1,4 @@
 import boto3
-from dotenv import load_dotenv
-from pathlib import Path
 from typing import List, Dict
 
 
@@ -9,3 +7,14 @@ class AWS_Manager:
         self.ec2_resource = boto3.resource("ec2",
                                            aws_access_key_id=aws_access_key_id,
                                            aws_secret_access_key=aws_secret_access_key, region_name="us-west-2")
+
+    def get_instance_info(self, instance_id: str) -> Dict[str, str]:
+        instance = self.ec2_resource.Instance(instance_id)
+        return {
+            "ID": instance.id,
+            "Name": instance.tags[0]["Value"] if instance.tags != None else "",
+            "State": instance.state["Name"],
+            "AMI": instance.image.id,
+            "Type": instance.instance_type,
+            "Public IPv4 address": instance.public_ip_address
+        }
