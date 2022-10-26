708e8c47f28187696669b1afe1ec0f5094dc5809
Itay.Talias <itay.talias@gmail.com>
2022-10-26 10:44:31 +0300

    userf class

---
+++ CODE/BACK/user_class.py
@@ -0,0 +1,9 @@
+class User():
+    def __init__(self, username: str,
+                 email: str,
+                 full_name: str,
+                 password: str):
+        self.username = username
+        self.email = email
+        self.full_name = full_name
+        self.password = password
