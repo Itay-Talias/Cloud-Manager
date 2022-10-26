7442e9afcf16be1700125f7c2ec4eb82d0231f28
yagelpinhas <yagel250@gmail.com>
2022-10-27 02:43:50 +0300

    changed cloud managers controller

---
+++ CODE/BACK/cloud_managers_controller.py
@@ -0,0 +1,13 @@
+from AWS_manager import AWS_Manager
+
+class Cloud_managers_controller:
+    def __init__(self):
+        self.client_cloud_connections = {}
+
+    def add_client(self, client_name: str, client_access_key_id: str, client_access_secret_key_id: str) -> None:
+        aws_manager = AWS_Manager(
+            aws_access_key_id=client_access_key_id, aws_secret_access_key=client_access_secret_key_id)
+        self.client_cloud_connections[client_name]={"connection": aws_manager,"audit":None}
+
+    def get_client_instance_by_client_name(self, client_name: str) -> AWS_Manager:
+        return self.client_cloud_connections[client_name]["connection"]
