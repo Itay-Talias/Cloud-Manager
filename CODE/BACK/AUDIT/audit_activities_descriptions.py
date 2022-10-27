f2ac4b94eb6fbda3a81e8c0ebf54a4573c4ddcd7
dekellevy99 <dekelllevy@gmail.com>
2022-10-27 04:44:27 +0300

    create a file contains constants descriptions

---
+++ CODE/BACK/AUDIT/audit_activities_descriptions.py
@@ -0,0 +1,4 @@
+INSTANCE_METHOD = "{} change state of instance with id {} to {}"
+INSTANCES_FILTER_SUCCESS = "{} filter instances by STATES = [{}], TYPES = [{}]"
+INSTANCES_FILTER_FAILED = "{} failed to filter instances by STATES = [{]], TYPES = [{}]"
+USER_LOGIN = "{} successed login"
