77c300f38e9df94b17e4402b1b0e7289d45127f6
yagelpinhas <yagel250@gmail.com>
2022-10-27 03:03:45 +0300

    created audit controller and changed aws manager instance wherever it was found in the server

---
+++ CODE/BACK/audit_controller.py
@@ -0,0 +1,3 @@
+class Audit_Controller:
+    def __init__(self, aws_access_key_id: str, aws_secret_access_key: str):
+        a=5
--- CODE/BACK/server.py
+++ CODE/BACK/server.py
@@ -13,6 +13,8 @@ from AUTH.authentication import get_current_user, get_user_from_db
 from AUTH.user_class import User
 from passlib.context import CryptContext
 from config import AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY
+from cloud_managers_controller import Cloud_managers_controller
+from audit_controller import Audit_Controller
 
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
@@ -20,8 +22,8 @@ app = FastAPI()
 app.mount("/FRONT", StaticFiles(directory="FRONT"), name="FRONT")
 
 acceptable_states = ["running", "stopped", "terminated"]
-acceptable_types = ["t2.micro", "t1.micro"],
-
+acceptable_types = ["t2.micro", "t1.micro"]
+cloud_managers_controller: Cloud_managers_controller = Cloud_managers_controller({"cyberark":{"connection":AWS_Manager(AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY) ,"audit": Audit_Controller()}})
 
 def check_params(params_received: List[str], acceptable_params: List[str]):
     if len(params_received) == 0:
@@ -51,7 +53,7 @@ async def login(response: Response, form_data: OAuth2PasswordRequestForm = Depen
 
 @app.get("/instances/")
 async def get_instances(states: str = "", types: str = "", response: Response = None, current_user: User = Depends(get_current_user)) -> List:
-    aws_manager = AWS_Manager(AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY)
+    aws_manager = cloud_managers_controller.get_client_instance_by_client_name(current_user.company)
     if states == "" and types == "":
         results = aws_manager.get_all_instances()
     elif check_params(params_received=states, acceptable_params=acceptable_states) == False or check_params(params_received=types, acceptable_params=acceptable_types) == False:
@@ -72,7 +74,7 @@ async def get_instances(states: str = "", types: str = "", response: Response = 
 async def operate(instance_id, request: Request, response: Response, current_user: User = Depends(get_current_user)):
     req = await request.json()
     new_state = req["state"]
-    aws_manager = AWS_Manager(AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY)
+    aws_manager = cloud_managers_controller.get_client_instance_by_client_name(current_user.company)
     operations_dict = {"terminated": aws_manager.terminate_instance, "stopped": aws_manager.stop_instance,
                        "running": aws_manager.start_instance, "reboot": aws_manager.reboot_instance}
     operations_dict[new_state](instance_id)
