438a5332d4955ca05002fbb1ce03cf5395f32b73
dekellevy99 <dekelllevy@gmail.com>
2022-10-27 05:10:44 +0300

    audit diffrenet operations on the server

--- CODE/BACK/server.py
+++ CODE/BACK/server.py
@@ -1,3 +1,5 @@
+from AUDIT.Audit_Manager import Audit_Manager
+from AUDIT import audit_activities_descriptions
 import mock_data
 from unittest import result
 from requests import Response
@@ -13,7 +15,9 @@ from AUTH.authentication import get_current_user, get_user_from_db
 from AUTH.user_class import User
 from passlib.context import CryptContext
 from config import AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY
+from datetime import datetime
 
+audit_manager = Audit_Manager("cyberark")
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 app = FastAPI()
@@ -39,23 +43,29 @@ async def login(response: Response, form_data: OAuth2PasswordRequestForm = Depen
         raise HTTPException(
             status_code=400, detail="Incorrect username or password or company")
     user = User(**user_dict)
-    print(form_data.client_secret, user.company)
     if (not pwd_context.verify(form_data.password, user.password)) or form_data.client_secret != user.company:
         raise HTTPException(
             status_code=400, detail="Incorrect username or password or company")
     token = user.username + " " + user.company
     response.set_cookie(key="Authorization", value="Bearer %s " %
                         token, httponly=True)
+    cur_time = datetime.now()
+    description = audit_activities_descriptions.USER_LOGIN.format(user.username)
+    audit_manager.write_activity(cur_time, description)
     return {"access_token": token, "token_type": "bearer"}
 
 
 @app.get("/instances/")
 async def get_instances(states: str = "", types: str = "", response: Response = None, current_user: User = Depends(get_current_user)) -> List:
     aws_manager = AWS_Manager(AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY)
+    cur_time = datetime.now()
+    username = current_user.split(" ")[0]
+    description = ""
     if states == "" and types == "":
         results = aws_manager.get_all_instances()
     elif check_params(params_received=states, acceptable_params=acceptable_states) == False or check_params(params_received=types, acceptable_params=acceptable_types) == False:
         response.status_code = status.HTTP_400_BAD_REQUEST
+        description = audit_activities_descriptions.INSTANCES_FILTER_FAILED.format(username, states, types)
         return {"Error": "states or types are invalid"}
     elif states == "":
         results = aws_manager.filter_instances_by_types(types=types.split("_"))
@@ -65,6 +75,8 @@ async def get_instances(states: str = "", types: str = "", response: Response = 
         if types != "":
             results = list(
                 filter(lambda instance: instance["Type"] in types.split("_"), results))
+    description = audit_activities_descriptions.INSTANCES_FILTER_SUCCESS.format(username, states, types)
+    audit_manager.write_activity(cur_time, description)
     return results
 
 
@@ -76,6 +88,11 @@ async def operate(instance_id, request: Request, response: Response, current_use
     operations_dict = {"terminated": aws_manager.terminate_instance, "stopped": aws_manager.stop_instance,
                        "running": aws_manager.start_instance, "reboot": aws_manager.reboot_instance}
     operations_dict[new_state](instance_id)
+    cur_time = datetime.now()
+    username = current_user.split(" ")[0]
+    description = audit_activities_descriptions.INSTANCE_METHOD.format(username, instance_id, new_state)
+    audit_manager.write_activity(cur_time, description)
+
 
 
 @app.get("/")
