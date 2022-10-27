c3afe97cb93a73103a7ea029cdbb46063fe7a857
dekellevy99 <dekelllevy@gmail.com>
2022-10-27 05:08:39 +0300

    change acitivies descriptions including new line

--- CODE/BACK/AUDIT/audit_activities_descriptions.py
+++ CODE/BACK/AUDIT/audit_activities_descriptions.py
@@ -1,4 +1,4 @@
-INSTANCE_METHOD = "{} change state of instance with id {} to {}"
-INSTANCES_FILTER_SUCCESS = "{} filter instances by STATES = [{}], TYPES = [{}]"
-INSTANCES_FILTER_FAILED = "{} failed to filter instances by STATES = [{]], TYPES = [{}]"
-USER_LOGIN = "{} successed login"
+INSTANCE_METHOD = "{} change state of instance with id {} to {}\n"
+INSTANCES_FILTER_SUCCESS = "{} filter instances by STATES = [{}], TYPES = [{}]\n"
+INSTANCES_FILTER_FAILED = "{} failed to filter instances by STATES = [{]], TYPES = [{}]\n"
+USER_LOGIN = "{} successed login\n"
